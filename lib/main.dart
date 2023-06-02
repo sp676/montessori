535694647fde0627ad2195ffbd8e6010c5d1b9e6
Santosh <santosh.poudel676@gmail.com>
2023-06-02 15:48:42 +0545

    null

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         //   ],
         //   child:
         MaterialApp(
-          debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       title: 'Montessori App',
       theme: appThemeData,
       initialRoute: Routes.splash,
--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -9,51 +9,70 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: MyColors.secondaryColor,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                'assets/images/logo.jpg', // Replace with your logo image path
-                width: 300,
-                height: 300,
-              ),
-              Image.asset('assets/images/children1.jpg', height: 350,),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  padding: const EdgeInsets.all(35),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(18),
-                    color: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: ListView(
+        children: [
+          Container(
+            color: MyColors.secondaryColor,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Visibility(
+                  visible: MediaQuery.of(context).viewInsets.bottom == 0,
+                  child: Image.asset(
+                    'assets/images/logo.jpg',
+                    width: 300,
+                    height: 300,
                   ),
-                  constraints: const BoxConstraints(
-                    maxHeight: double.infinity,
-                    maxWidth: double.infinity,
-                  ),
-                  child: Column(
-                    children: [
-                      const Row(
-                        children: [
-                          Text(
-                            "SIGN IN",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Image.asset(
+                  'assets/images/children1.jpg',
+                  height: 350,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(35),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        const Row(
+                          children: [
+                            Text(
+                              "SIGN IN",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              fillColor: MyColors.secondaryColor,
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
                           ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: TextFormField(
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          obscureText: true,
                           decoration: InputDecoration(
-                            hintText: 'Email',
+                            hintText: 'Password',
                             fillColor: MyColors.secondaryColor,
                             filled: true,
                             border: OutlineInputBorder(
@@ -62,38 +81,48 @@ class LoginScreen extends StatelessWidget {
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          fillColor: MyColors.secondaryColor,
-                          filled: true,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: BorderSide.none,
-                          ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.qr_code,
+                            ),
+                            const Text(
+                              'Scan QR',
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: MyColors.buttonColor,
+                                fixedSize: const Size(100, 25),
+                              ),
+                              onPressed: () {
+                                Navigator.pushNamed(context, Routes.home);
+                              },
+                              child: const Text(
+                                'LOGIN',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(backgroundColor: MyColors.buttonColor,
-                        fixedSize: const Size(100, 25)),
-                        onPressed: () {
-                          Navigator.pushNamed(context, Routes.home);
-                        },
-                        child: const Text('Login'),
-                      ),
-                    ],
+                        const SizedBox(height: 15,),
+                        const Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.blue),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
