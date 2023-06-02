55f1441f9a110caa7b85f071f41701cd2e2917a2
Santosh <santosh.poudel676@gmail.com>
2023-06-02 13:07:09 +0545

    some issues fixed

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:montessori/screens/authentication/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
